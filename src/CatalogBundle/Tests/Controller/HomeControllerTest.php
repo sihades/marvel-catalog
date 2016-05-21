ba72f51eed2fc2536cf60256c320be66b629d7ab
sihades <ivan_ra10@hotmail.com>
2016-05-21 02:04:55 -0500

    MC 2: Configured phpunit, behat, php-cs-fixer, guzzle

--- src/CatalogBundle/Tests/Controller/HomeControllerTest.php
+++ src/CatalogBundle/Tests/Controller/HomeControllerTest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Tests\AppBundle\Controller;
+namespace Sihades\Marvel\CatalogBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class HomeControllerTest extends WebTestCase
 {
     public function testIndex()
     {
@@ -13,6 +13,6 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('Marvel Catalog', $crawler->filter('h1')->text());
     }
 }
